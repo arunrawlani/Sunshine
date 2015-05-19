ebaffc477daea184a006c8ebace8e4594e79a6e1
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-05-19 17:31:58 -0400

    First commit

--- app/src/main/java/com/example/arunrawlani/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/arunrawlani/sunshine/app/MainActivityFragment.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 
 /**
@@ -20,9 +22,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Create an array called data with the weather info for the week
+        String[] data= {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        //Create a List and add the whole array to the ArrayList
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
         return inflater.inflate(R.layout.fragment_main, container, false);
 
-        ArrayList<String> List = new ArrayList<String>();
-                ArrayList.add("Today-Suuny-86/30");
     }
 }
