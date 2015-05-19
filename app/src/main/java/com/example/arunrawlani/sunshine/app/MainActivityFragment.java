63393767e2cfc2dd1b8ab77388695ab16c003611
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-05-19 16:07:09 -0400

    first commit

---
+++ app/src/main/java/com/example/arunrawlani/sunshine/app/MainActivityFragment.java
@@ -0,0 +1,28 @@
+package com.example.arunrawlani.sunshine.app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> List = new ArrayList<String>();
+                ArrayList.add("Today-Suuny-86/30");
+    }
+}
